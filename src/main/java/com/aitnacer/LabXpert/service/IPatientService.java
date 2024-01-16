2ebd8110c8683306a14b348ce31f53a113f4e100
kawtarmouslim <kawtharmouslim33@gmail.com>
2024-01-16 11:52:58 +0100

    test

--- src/main/java/com/aitnacer/LabXpert/service/IPatientService.java
+++ src/main/java/com/aitnacer/LabXpert/service/IPatientService.java
@@ -1,4 +1,17 @@
 package com.aitnacer.LabXpert.service;
 
+import com.aitnacer.LabXpert.dtos.PatientDto;
+import com.aitnacer.LabXpert.dtos.UtilisateurDto;
+
+import java.util.List;
+
 public interface IPatientService {
+    List<PatientDto> getAllPatient();
+    PatientDto getPatientById(Long id);
+    PatientDto createPatient(PatientDto patientDto);
+    PatientDto updatePatient(Long id ,PatientDto patientDto);
+
+
+
+    void deleteUtilisateur(Long id);
 }
